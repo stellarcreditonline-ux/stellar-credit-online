d649660e9ff1cbce860b4cd48e4248367dce28ef
stellarcreditonline-ux <stellarcreditonline@gmail.com>
2025-09-03 04:26:11 +0100

    Create server.js

---
+++ backend/backend/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = 5000;
+
+app.get('/', (req, res) => {
+  res.send('Stellar Credit Online Backend Running 🚀');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});￼Enter
